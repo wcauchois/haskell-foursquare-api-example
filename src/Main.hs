2a7b51b25256cb72bd8eb7363c02eea2e00b1866
Bill Cauchois <wcauchois@gmail.com>
2013-07-21 21:39:13 +0000

    cool! reverse geocode and then print out foursquare venues. neat.

--- src/Main.hs
+++ src/Main.hs
@@ -14,24 +14,14 @@ targetAddress = "568 Broadway, New York, NY"
 -- | The main entry point.
 main :: IO ()
 main =
-  do geocodeResponse <- (callJsonEndpoint $ GeocodeEndpoint targetAddress False :: IO GeocodeResponse)
-     print geocodeResponse
-{-
   do putStrLn "API key?"
      apiKey <- getLine
      putStrLn "API secret?"
      apiSecret <- getLine
      let creds = FoursquareCredentials apiKey apiSecret
-     let ll = (40.774848,-73.953368)
-     venuesTrendingResponse <- (callJsonEndpoint $ VenuesTrendingEndpoint ll Nothing Nothing `authorizeWith` creds :: IO VenuesTrendingResponse)
-     print venuesTrendingResponse
-     -}
 
---
-
-     --geocodeResponse <- callJsonEndpoint (GeocodeEndpoint targetAddress False)
---main = do putStrLn "YO WHATS UP"
-  --putStrLn $ show $ buildURI $ VenuesTrending (2.0, 3.0) Nothing Nothing
-  --res <- simpleHTTP (getRequest "http://www.haskell.org/") >>= fmap (take 100) . getResponseBody
-  --B.putStrLn $ renderQuery False [(B.pack "hey", Just $ B.pack "you")]
-  --putStrLn $ show $ buildURI $ VenuesTrending (2.0, 3.0) Nothing Nothing
+     (GeocodeResponse latLng) <- (callJsonEndpoint $ GeocodeEndpoint targetAddress False :: IO GeocodeResponse)
+     let venuesTrendingEndpoint = VenuesTrendingEndpoint latLng Nothing Nothing `authorizeWith` creds
+     (VenuesTrendingResponse venues) <- (callJsonEndpoint venuesTrendingEndpoint :: IO VenuesTrendingResponse)
+     let printVenue v = putStrLn $ "- " ++ name v
+     mapM_ printVenue venues
